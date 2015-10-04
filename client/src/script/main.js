1948cd79a9e7ff371cd7432351137077f0118414
leader22 <y.sugiura.0316@gmail.com>
2015-10-04 23:29:16 +0900

    ちょっとclient整理

--- client/src/script/main.js
+++ client/src/script/main.js
@@ -1,44 +1,13 @@
-(function(global) {
-  'use strict';
+'use strict';
 
-  var axios = require('axios');
-  var document = global.document;
+var pathname = location.pathname;
 
-  var $id = document.getElementById.bind(document);
-
-  var Search = function() {
-    this.init.apply(this);
-  };
-  Search.prototype = {
-    constructor: Search,
-    init: function() {
-      this.$ = {
-        query: $id('query'),
-        exec:  $id('exec'),
-        res:   $id('res')
-      };
-      this.$.exec.addEventListener('click', this.execSearch.bind(this), false);
-    },
-    execSearch: function() {
-      var that = this;
-      var query = this.$.query.value;
-      if (query.length === 0) { return; }
-
-      axios.get('/api/search', {
-        params: {
-          q: query
-        }
-      })
-      .then(that._handleRes.bind(that))
-      .catch(function (res) {
-        console.error(res);
-      });
-    },
-    _handleRes: function(res) {
-      this.$.res.textContent = JSON.stringify(res.data, null, 2);
-    }
-  };
-
+switch (pathname) {
+case '/player':
+  break;
+case '/':
+default:
+  var Search = require('./component/search');
   new Search();
-
-}(window));
+  break;
+}
