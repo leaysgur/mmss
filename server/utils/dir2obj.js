fbea281505c5cf012ebac86fbc44cbd831d29e97
leader22 <y.sugiura.0316@gmail.com>
2015-10-05 01:34:57 +0900

    ゴミファイルを無視するように

--- server/utils/dir2obj.js
+++ server/utils/dir2obj.js
@@ -18,7 +18,7 @@ module.exports = function(dir, fn) {
       files.map(function (file) {
         return path.join(baseDir, file);
       }).filter(function (file) {
-        /// ディレクトリなら再帰
+        // ディレクトリなら再帰
         if(fs.statSync(file).isDirectory()) {
           _walk(file, function(err, res) {
             // [!] これがディレクトリのデータ構造
@@ -36,10 +36,15 @@ module.exports = function(dir, fn) {
 
         return fs.statSync(file).isFile();
       }).forEach(function (file) {
-        // [!] これがファイルのデータ構造
-        results.push({
-          n: path.basename(file)
-        });
+        var pathArr = file.split('/');
+        var fileName = pathArr[pathArr.length - 1];
+        // .からはじまるゴミファイルは無視
+        if (fileName.charAt(0) !== '.') {
+          // [!] これがファイルのデータ構造
+          results.push({
+            n: path.basename(file)
+          });
+        }
 
         // 終わり
         if (!--pending) {
