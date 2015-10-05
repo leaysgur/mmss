a5dbf1698ac2a5c8ddfcf57ec0a15cee88b8dccf
leader22 <y.sugiura.0316@gmail.com>
2015-10-06 01:08:40 +0900

    wipwip

--- client/src/script/app/index.vue
+++ client/src/script/app/index.vue
@@ -1,7 +1,7 @@
 <template>
   <h2>アーティスト検索</h2>
   <p class="content">
-    アーティスト名を入力すると、持ってるアルバムがわかります
+    アーティスト名を入力すると、持ってるアルバムがわかります。
   </p>
   <div class="content">
     <search-view></search-view>
