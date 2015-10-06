b29bc9f6f0c2bb8cebc80780dceaeb0558cfcd03
leader22 <y.sugiura.0316@gmail.com>
2015-10-06 15:30:01 +0900

    表示ラベル

--- client/src/script/components/play-list.vue
+++ client/src/script/components/play-list.vue
@@ -2,6 +2,14 @@
 .playList-item {
   display: flex;
 
+  &-mLabel {
+    padding-bottom: 4px;
+    margin-bottom: 8px;
+    border-bottom: 1px solid #eee;
+    font-size: .8em;
+    font-weight: bold;
+  }
+
   &-trackNo {
     width: 28px;
     margin-right: 8px;
@@ -16,6 +24,12 @@
 <template>
 <ul class="playList">
   <li v-show="items.length === 0">曲を選択してください</li>
+  <li v-show="items.length !== 0" class="playList-item playList-item-mLabel">
+    <div class="playList-item-trackNo">No</div>
+    <div class="playList-item-trackInfo">曲名</div>
+    <div class="playList-item-trackInfo">アルバム名</div>
+    <div class="playList-item-trackInfo">アーティスト名</div>
+  </li>
   <li class="playList-item clickableLi" v-repeat="track in items" v-on="click: onClickTrack(track.n)">
     <div class="playList-item-trackNo">{{_getTrackNo(track.t.tr)}}</div>
     <div class="playList-item-trackInfo">{{track.t.ti}}</div>
